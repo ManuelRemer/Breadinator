88a42a66eb7beaf21bf8801e2c8240bfa2a45eb2
Manuel Remer <manuelremer@web.de>
2022-10-19 19:50:55 +0200

    notFound_MW

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 const connectToDB = require("./connect");
 const errorHandler = require("./middleware/error_handler_MW");
+const notFound = require("./middleware/not_found_MW");
 const router = require("./routes/recipeRoutes");
 
 // middelware
@@ -20,6 +21,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.use(errorHandler);
+app.use(notFound);
 
 // start server + connect to db
 const start = async (port) => {
