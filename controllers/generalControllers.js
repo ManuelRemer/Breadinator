875d524e3027520db62a35755eda0bdc487492da
Manuel Remer <manuelremer@web.de>
2022-10-19 18:06:44 +0200

    all crud actions handled with tryCatch and basic error handling

--- controllers/generalControllers.js
+++ controllers/generalControllers.js
@@ -2,7 +2,6 @@ const recipeModel = require("../models/IngredientsLists");
 
 const addItem = async (req, res) => {
   const { body } = req;
-
   try {
     await recipeModel.create(body);
     res.status(200).json({ success: true });
@@ -20,16 +19,38 @@ const getAllItems = async (req, res) => {
   }
 };
 
-const getItem = (req, res) => {
-  res.send("get single item");
+const getItem = async (req, res) => {
+  const { params } = req;
+  try {
+    const item = await recipeModel.findOne({ _id: params.recipe });
+    res.status(200).json({ item });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 };
 
-const deleteItem = (req, res) => {
-  res.send("delete item");
+const deleteItem = async (req, res) => {
+  const { recipe } = req.params;
+  try {
+    await recipeModel.deleteOne({ _id: recipe });
+    res.status(200).json({ success: true, msg: "deleted:" + recipe });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 };
 
-const updateItem = (req, res) => {
-  res.send("updateItem item");
+const updateItem = async (req, res) => {
+  try {
+    const { recipe } = req.params;
+    const body = req.body;
+    await recipeModel.findOneAndUpdate({ _id: recipe }, { ...body });
+    res.status(200).json({ success: true, msg: "updated: " + recipe });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 };
 
 module.exports = { addItem, getAllItems, deleteItem, updateItem, getItem };
